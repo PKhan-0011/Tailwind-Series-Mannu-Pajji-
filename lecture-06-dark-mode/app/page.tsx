38ebb55f6da7a2b8d158fbee33c1e085428bf71c
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-11-02 13:25:20 +0530

    code push

---
+++ lecture-06-dark-mode/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="relative h-screen w-full flex items-center justify-center bg-gray-100">
+      <div className="absolute  h-90 w-80 bg-white shadow-2xl border-neutral-200 rounded-xl"></div>
+    </div>
+  );
+}
