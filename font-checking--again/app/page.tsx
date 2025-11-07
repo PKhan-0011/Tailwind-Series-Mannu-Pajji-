f849ec04cce1a4131f937bdd781fda46ba631205
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-11-07 12:58:00 +0530

    code push

--- font-checking--again/app/page.tsx
+++ font-checking--again/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center  gap-2 mt-10 text-3xl">
+    <div className="flex items-center justify-center  gap-2 mt-10">
       <h1>Aur mittr kya haa; hai</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea veritatis
--- font-checking/app/page.tsx
+++ font-checking/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" flex items-center justify-center mt-10 gap-4">
+    <div className="flex items-center justify-center mt-10 gap-4">
       <h1>hi ThemeProvider</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat</p>
     </div>
