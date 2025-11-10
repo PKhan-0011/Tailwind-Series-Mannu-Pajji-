1478ce7eda65a57f0986dc950e47c1f5dc4d0e13
PKhan-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-11-10 12:18:35 +0530

    code push

---
+++ final-project-mannu-paji/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="items-center justify-center flex bg-red-500">
+      <h1 className="">Hi there</h1>
+    </div>
+  );
+}
