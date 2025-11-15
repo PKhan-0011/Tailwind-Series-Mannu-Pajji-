e55959b1f997c2ec82c9069f91985dc269ca7123
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-11-15 17:30:42 +0530

    code push

---
+++ final-project-mannu-paji/Components/CardSkeleton/ThirdSkeleton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { DottedGlowBackground } from "../ui/dotted-glow-background";
+
+const ThirdSkeleton = () => {
+  return (
+    <div>
+      <DottedGlowBackground
+        className="pointer-events-none mask-radial-to-90% mask-radial-at-center"
+        opacity={1}
+        gap={10}
+        radius={1.6}
+        colorLightVar="--color-neutral-500"
+        glowColorLightVar="--color-neutral-600"
+        colorDarkVar="--color-neutral-500"
+        glowColorDarkVar="--color-sky-800"
+        backgroundOpacity={0}
+        speedMin={0.3}
+        speedMax={1.6}
+        speedScale={1}
+      />
+    </div>
+  );
+};
+
+export default ThirdSkeleton;
