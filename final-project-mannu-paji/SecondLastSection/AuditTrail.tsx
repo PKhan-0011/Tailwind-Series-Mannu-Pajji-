01167e568273f7a393f278d6a58f68994c85e497
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-11-21 23:56:39 +0530

    code push

--- final-project-mannu-paji/SecondLastSection/AuditTrail.tsx
+++ final-project-mannu-paji/SecondLastSection/AuditTrail.tsx
@@ -1,114 +1,3 @@
-// "use clinet";
-// import React from "react";
-// import Container from "@/Components/Container";
-// import { motion } from "motion/react";
-// import { cn } from "@/lib/utils";
-// import FirstCard from "./CardSkeleton02/FirstCard";
-// import SecondCard from "./CardSkeleton02/SecondCard";
-// import ThirdCard from "./CardSkeleton02/ThirdCard";
-// import SecondCardSkeleton from "@/Components/CardSkeleton/SecondCard/SecondCardSkeleton";
-
-// const FeatureSecondary = () => {
-//   return (
-//     <section className="relative overflow-hidden pt-10 md:pt-20 lg:pt-32">
-//       <Container className="mb-2">
-//         <div className="grid grid-cols-1 divide-x divide-neutral-200 border-y border-neutral-200 md:grid-cols-2 dark:divide-neutral-800 dark:border-neutral-800">
-//           <div className="p-4">
-//             <h2 className="text-lg font-bold text-neutral-800">
-//               Agents Studio
-//             </h2>
-//             <CardDescription className="">
-//               Design, launch and customize AI agents for maketing, sales,
-//               support and ops, built around your workflows
-//             </CardDescription>
-//             <CardSkeleton>
-//               {""}{" "}
-//               <CardSkeleton01>
-//                 <FirstCard />
-//                 <SecondCard />
-//                 <ThirdCard />
-//               </CardSkeleton01>
-//             </CardSkeleton>
-//           </div>
-//           <div className="p-4">
-//             <h2 className="text-lg font-bold text-neutral-800">
-//               Ai Agents Orchestration..
-//             </h2>
-//             <CardDescription className="">
-//               Design, launch and customize AI agents for maketing, sales,
-//               support and ops, built around your workflows
-//             </CardDescription>
-
-//             <CardSkeleton>
-//               <SecondCardSkeleton />
-//             </CardSkeleton>
-//           </div>
-//         </div>
-//       </Container>
-//     </section>
-//   );
-// };
-
-// export const CardDescription = ({
-//   children,
-//   className,
-// }: {
-//   children: React.ReactNode;
-//   className: string;
-// }) => {
-//   return (
-//     <div
-//       className={cn(
-//         "mt-2 max-w-sm text-sm text-balance text-neutral-600",
-//         className,
-//       )}
-//     >
-//       {children}
-//     </div>
-//   );
-// };
-
-// export const CardSkeleton = ({
-//   className,
-//   children,
-// }: {
-//   className?: string;
-//   children: React.ReactNode;
-// }) => {
-//   return (
-//     <div
-//       className={cn(
-//         "relative inset-x-0 flex h-80 flex-col overflow-hidden perspective-distant sm:h-60 md:h-100",
-//         className,
-//       )}
-//     >
-//       {children}
-//       {/* ye dhyan rtakh like kii iske andar sara data skeleton ayega card hai teen wo okkh!..*/}
-//     </div>
-//   );
-// };
-
-// // abb yha s mai actully carrd sakelton s chote chote components banunga okh!..
-
-// export const CardSkeleton01 = ({
-//   className,
-//   children,
-// }: {
-//   className?: string;
-//   children?: React.ReactNode;
-// }) => {
-//   return (
-//     <div
-//       className={cn(
-//         "absolute inset-0 mx-auto mt-4 h-full w-full max-w-sm flex-1 rounded-t-3xl border border-neutral-200 bg-neutral-100 p-px",
-//         className,
-//       )}
-//     >
-//       {children}
-//     </div>
-//   );
-// };
-
 "use client";
 
 import React from "react";
@@ -124,6 +13,8 @@ import { Button } from "@/Components/ui/button";
 import { ClockFading } from "lucide-react";
 import { LoaderCircle } from "lucide-react";
 import { motion } from "motion/react";
+import { Search } from "lucide-react";
+import BelowAudit from "./BelowAudit";
 
 const AuditTrail = () => {
   return (
@@ -158,6 +49,8 @@ const AuditTrail = () => {
             </CardDescription>
           </div>
         </div>
+        {/* Yha s second part srart hoga okkh! niche wle elemnst okkh!*/}
+        <BelowAudit />
       </Container>
     </section>
   );
@@ -205,6 +98,7 @@ export const CardMaterials = () => {
     <motion.div
       className="h-full cursor-pointer divide-y divide-neutral-200 overflow-hidden rounded-2xl border-2 border-neutral-200 bg-white"
       initial={{
+        zIndex: 1,
         y: 0,
         opacity: 0,
       }}
@@ -217,8 +111,9 @@ export const CardMaterials = () => {
         ease: "easeInOut",
       }}
       whileHover={{
-        y: -10,
-        scale: 1,
+        zIndex: 20,
+        scale: 1.02,
+        y: -4,
       }}
     >
       <div className="flex items-center gap-2 border-b p-2 pl-6">
---
+++ final-project-mannu-paji/SecondLastSection/BelowAudit.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Button } from "@/Components/ui/button";
+import { CardDescription } from "./AuditTrail";
+import { Search } from "lucide-react";
+import { Rocket } from "lucide-react";
+import { Pointer } from "lucide-react";
+import { SunMedium } from "lucide-react";
+import { UserRound } from "lucide-react";
+
+const BelowAudit = () => {
+  return (
+    <div className="grid grid-cols-1 divide-x divide-neutral-200 border-y border-neutral-200 md:grid-cols-2 dark:divide-neutral-800 dark:border-neutral-800">
+      <div className="flex flex-col gap-2 overflow-hidden">
+        <div className="flex flex-col gap-2 p-4">
+          <h1 className="text-xl font-bold text-neutral-800">Audit Trail</h1>
+          <CardDescription className="">
+            <p>
+              Track every agent action with full input-output <br /> visiblity
+              and timestamps.
+            </p>
+          </CardDescription>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 p-4">
+        <h1 className="text-xl font-bold text-neutral-800">Guardail Engine</h1>
+        <CardDescription className="">
+          <p>
+            Appliies brand, tone, and policy checks before output <br /> goes
+            live..
+          </p>
+        </CardDescription>
+
+        <div>
+          <div>
+            <Button className="bg-blue-100 text-blue-400">
+              <SunMedium size={20} /> Brand & Style
+            </Button>
+            <Button className="bg-green-100 text-green-400">
+              {" "}
+              <UserRound /> Compliance & policy
+            </Button>
+          </div>
+          <div>
+            <Button className="bg-purple-100 text-purple-400">
+              {" "}
+              <Rocket size={20} className="" /> Content Safety Fiilters
+            </Button>
+            <Button className="bg-gray-300 text-gray-700 hover:bg-white">
+              {" "}
+              <Pointer size={20} className="" /> Approval Triggers
+            </Button>
+          </div>
+          <div className="">
+            <Button variant="outline" className="bg-pink-100 text-red-400">
+              <Search /> Output Quality Checks
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BelowAudit;
