a735f307514775b83bfa59cd16f02119e101ebb7
PKHAN-0011 <184125762+PKhan-0011@users.noreply.github.com>
2025-11-22 15:58:20 +0530

    code push

--- final-project-mannu-paji/SecondLastSection/BelowAudit.tsx
+++ final-project-mannu-paji/SecondLastSection/BelowAudit.tsx
@@ -6,6 +6,8 @@ import { Rocket } from "lucide-react";
 import { Pointer } from "lucide-react";
 import { SunMedium } from "lucide-react";
 import { UserRound } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Undo } from "lucide-react";
 
 const BelowAudit = () => {
   return (
@@ -19,6 +21,30 @@ const BelowAudit = () => {
               and timestamps.
             </p>
           </CardDescription>
+        </div>
+        <div
+          className="mt-10 h-80 w-120 bg-[linear-gradient(to_right,rgba(0,0,0,0.10)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.10)_1px,transparent_1px)] bg-[size:24px_24px] dark:bg-[linear-gradient(to_right,rgba(255,255,255,0.06)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.06)_1px,transparent_1px)]"
+          style={{
+            transform: "rotateY(20deg) rotateX(10deg) rotateZ(30deg)",
+          }}
+        >
+          <div className="relative z-1 flex h-80 w-120 items-center justify-center gap-20">
+            <div className="flex items-center gap-2 rounded-full border border-orange-500 bg-orange-100 p-1 px-4 py-2 font-medium text-orange-500">
+              {" "}
+              <Search />
+              <span>Processing...</span>{" "}
+            </div>
+
+            <div className="flex items-center gap-2 rounded-full border border-green-500 bg-green-100 p-1 px-4 py-2 font-medium text-green-500">
+              {" "}
+              <SunMedium />
+              <span>FeedBack...</span>{" "}
+            </div>
+          </div>
+
+          <div className="text-gray-400">
+            <Undo />
+          </div>
         </div>
       </div>
       {/*Ye jo hai wo like 2nd wala part hai okkh!..*/}
